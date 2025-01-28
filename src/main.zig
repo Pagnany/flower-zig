d5db222960aaabff9161c4e250e5193a7f3d65c5
Eric Schmale <eric.schmale@gmx.de>
2025-01-28 20:49:47 +0100

    http test

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,21 @@
-// raylib-zig (c) Nikolas Wipper 2023
-
 const rl = @import("raylib");
+const std = @import("std");
 
 pub fn main() anyerror!void {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const screenWidth = 1280;
     const screenHeight = 720;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - mouse input");
-    defer rl.closeWindow(); // Close window and OpenGL context
+    rl.initWindow(screenWidth, screenHeight, "Flower");
+    defer rl.closeWindow();
 
     var ballPosition = rl.Vector2.init(-100, -100);
     var ballColor = rl.Color.dark_blue;
 
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    rl.setTargetFPS(60);
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         // Update
-        //----------------------------------------------------------------------------------
         ballPosition = rl.getMousePosition();
         ballPosition.x = @as(f32, @floatFromInt(rl.getMouseX()));
         ballPosition.y = @as(f32, @floatFromInt(rl.getMouseY()));
@@ -32,18 +27,13 @@ pub fn main() anyerror!void {
         } else if (rl.isMouseButtonPressed(.right)) {
             ballColor = rl.Color.dark_blue;
         }
-        //----------------------------------------------------------------------------------
 
         // Draw
-        //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.ray_white);
 
         rl.drawCircleV(ballPosition, 40, ballColor);
-
-        rl.drawText("move ball with mouse and click mouse button to change color", 10, 10, 20, rl.Color.dark_gray);
-        //----------------------------------------------------------------------------------
     }
 }
