f0d0978dec9236cac09094c80e31380d3fa75377
Eric Schmale <eric.schmale@gmx.de>
2025-02-16 12:56:38 +0100

    flower array

--- src/main.zig
+++ src/main.zig
@@ -5,9 +5,21 @@ const ctime = @cImport({
 });
 const http = @import("http.zig");
 
-const allocator = std.heap.c_allocator;
+const Flower = struct {
+    name: []u8,
+    description: []u8,
+    price: f32,
+    image: []u8,
+};
 
 pub fn main() !void {
+    const allocator = std.heap.c_allocator;
+
+    const my_flowers = try creat_flowers(allocator);
+    defer destroy_flowers(allocator, my_flowers);
+    for (my_flowers.items) |flower| {
+        std.debug.print("Name: {s}, Description: {s}, Price: {d}, Image: {s}\n", .{ flower.name, flower.description, flower.price, flower.image });
+    }
     // // Log in to the server
     // var login: http.LoginResponse = undefined;
     // {
@@ -33,7 +45,7 @@ pub fn main() !void {
 
     rl.setTargetFPS(60);
 
-    var ballPosition = rl.Vector2.init(-100, -100);
+    var mouse_pos = rl.Vector2.init(-100, -100);
 
     // Raylib load image and convert to texture
     const image = try rl.loadImage("resources/r.png"); // Loaded in CPU memory (RAM)
@@ -57,6 +69,7 @@ pub fn main() !void {
         const loop_time = rl.getTime();
         const delta_time = loop_time - prev_loop_time;
         _ = delta_time;
+        mouse_pos = rl.getMousePosition();
 
         // UPDATE
         // Timestamp
@@ -74,10 +87,11 @@ pub fn main() !void {
         }
 
         // Ballposition
-        ballPosition = rl.getMousePosition();
 
         // Mouse input
-        if (rl.isMouseButtonPressed(.left)) {} else if (rl.isMouseButtonPressed(.middle)) {} else if (rl.isMouseButtonPressed(.right)) {}
+        if (rl.isMouseButtonPressed(.left)) {
+            std.debug.print("Mouse left button pressed at x:{d}, y:{d}\n", .{ mouse_pos.x, mouse_pos.y });
+        } else if (rl.isMouseButtonPressed(.middle)) {} else if (rl.isMouseButtonPressed(.right)) {}
 
         // DRAW
         rl.beginDrawing();
@@ -96,7 +110,8 @@ pub fn main() !void {
             screenHeight / 2 - @divFloor(texture.height, 2),
             rl.Color.white,
         );
-        rl.drawTextureV(texture, ballPosition, rl.Color.white);
+        // rl.drawTextureV(texture, ballPosition, rl.Color.white);
+        rl.drawCircleV(mouse_pos, 10, rl.Color.red);
 
         // Timestamp at the top
         rl.drawText(timestamp, 1000, 10, 20, rl.Color.white);
@@ -115,4 +130,23 @@ fn get_timestamp(buffer: []u8) void {
     @memset(buffer, 0);
 
     _ = ctime.strftime(buffer.ptr, buffer.len, "%Y.%m.%d %H:%M:%S", timeinfo);
+}
+
+fn creat_flowers(alloc: std.mem.Allocator) !std.ArrayList(Flower) {
+    var flowers = std.ArrayList(Flower).init(alloc);
+
+    try flowers.append(Flower{ .name = try alloc.dupe(u8, "Rose"), .description = try alloc.dupe(u8, "Red"), .price = 1.0, .image = try alloc.dupe(u8, "resources/r.png") });
+    try flowers.append(Flower{ .name = try alloc.dupe(u8, "Tulip"), .description = try alloc.dupe(u8, "Yellow"), .price = 1.5, .image = try alloc.dupe(u8, "resources/t.png") });
+    try flowers.append(Flower{ .name = try alloc.dupe(u8, "Sunflower"), .description = try alloc.dupe(u8, "Yellow"), .price = 2.0, .image = try alloc.dupe(u8, "resources/s.png") });
+
+    return flowers;
+}
+
+fn destroy_flowers(alloc: std.mem.Allocator, flowers: std.ArrayList(Flower)) void {
+    for (flowers.items) |f| {
+        alloc.free(f.name);
+        alloc.free(f.description);
+        alloc.free(f.image);
+    }
+    flowers.deinit();
 }
